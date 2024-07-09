9a7d16370ee83ca5b0fa73957fe1494cc668286e
HEun0420 <usialeta@gmail.com>
2024-07-09 13:20:31 +0900

    request

---
+++ chap02-handler-method-lecture-source/src/main/java/com/ohgiraffers/handlermethod/Chap02HandlerMethodLectureSourceApplication.java
@@ -0,0 +1,17 @@
+package com.ohgiraffers.handlermethod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class Chap02HandlerMethodLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap02HandlerMethodLectureSourceApplication.class, args);
+    }
+
+}
