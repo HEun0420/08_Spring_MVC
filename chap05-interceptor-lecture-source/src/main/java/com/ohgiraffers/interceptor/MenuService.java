fb8249baa1efdf78047ac45fe676bad9b49ef66e
HEun0420 <usialeta@gmail.com>
2024-07-10 14:22:57 +0900

    interceptor

---
+++ chap05-interceptor-lecture-source/src/main/java/com/ohgiraffers/interceptor/MenuService.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.interceptor;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MenuService {
+
+    public void method(){
+        System.out.println("MenuService 호출됨....");
+    }
+
+
+}
