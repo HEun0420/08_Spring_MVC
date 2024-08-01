fb6d89e72606773d29895c26823cf1482d0f5936
HEun0420 <usialeta@gmail.com>
2024-08-01 18:13:21 +0900

    spring_security

---
+++ chap08-spring-security1-lecture-source/src/test/java/com/ohgiraffers/chap08springsecurity/Chap08SpringSecurity1LectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.chap08springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap08SpringSecurity1LectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
